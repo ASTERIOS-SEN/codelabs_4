8dcbc9f7a1da6423f077c44b20134bc8f3af9848
Danny Osmar Sevilla Martinez <dsevilla@ni.thedataage.com>
2023-04-18 22:18:05 -0600

    Final commit Exercise map

---
+++ bin/codelabs_4.dart
@@ -0,0 +1,5 @@
+import 'package:codelabs_4/codelabs_4.dart' as codelabs_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codelabs_4.calculate()}!');
+}
---
+++ test/codelabs_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:codelabs_4/codelabs_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
