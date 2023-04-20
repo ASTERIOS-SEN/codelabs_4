2fb207acd3d136cd50c55d3daaa82a9b03326712
Danny Osmar Sevilla Martinez <dsevilla@ni.thedataage.com>
2023-04-19 20:37:22 -0600

    upgrade codelabs

--- bin/codelabs_4.dart
+++ bin/codelabs_4.dart
@@ -1,5 +1,6 @@
 import 'package:codelabs_4/codelabs_4.dart' as codelabs_4;
 import 'type_null.dart';
+import 'null_aware_operators.dart';
 
 void main(List<String> arguments) {
   //Ejercicio: Parámetros de tipo anulable para genéricos "?"
@@ -9,5 +10,31 @@ void main(List<String> arguments) {
   // Ejercicio: Operador de aserción nula "!"
   null_assertion();
   print('');
-  
+
+  // Ejercicio: Operadores de acceso nulo "?."
+  returnStringNull();
+  print('');
+
+  //Ejercicio: Operadores de fusión nula "??"
+
+  //Ejercicio: Asignación definitiva "??="
+  asignacionDefinitiva();
+  print('');
+
+  //Ejercicio: Comprobación de nulos
+  returnGetLength();
+  print('');
+
+  //Ejercicio: Uso tardío
+  returnLate();
+  print('');
+
+  //Ejercicio: Referencias circulares tardías
+  returnLate2();
+  print('');
+
+  //Ejercicio: tarde y perezoso
+  returnLate3();
+  print('');
+
 }
